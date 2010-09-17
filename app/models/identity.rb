1ea5e40c6d0fb38fc10f26511dfd66ec1de87ecd
Flavio Castelli <fcastelli@novell.com>
2010-09-17 13:45:50 +0200

    improve validation

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,7 +1,7 @@
 class Identity < ActiveRecord::Base
   include UrlValidatorHelper
   belongs_to :user
-  validates_presence_of :name, :openid_server, :openid_delegate
+  validates_presence_of :name, :openid_server, :openid_delegate, :openid_url
   validates_uniqueness_of :name
-  validates_format_of_url :openid_server, :openid_delegate
+  validates_format_of_url :openid_server, :openid_delegate, :openid_url
 end
